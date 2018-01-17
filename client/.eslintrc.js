671d7d364e92bd23a9265313c53682834e33df68
xunmi <xunmi1@outlook.com>
2018-01-17 22:33:16 +0800

    项目重构

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,50 +1,86 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential', 'airbnb-base'],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // check if imports actually resolve
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: 'build/webpack.base.conf.js'
-      }
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    extends: ['plugin:vue/essential', 'airbnb-base'],
+    // required to lint *.vue files
+    plugins: [
+        'vue'
+    ],
+    // check if imports actually resolve
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: 'build/webpack.base.conf.js'
+            }
+        }
+    },
+    // add your custom rules here
+    rules: {
+        // don't require .vue extension when importing
+        'import/extensions': ['error', 'always', {
+            js: 'never',
+            vue: 'never'
+        }],
+        // disallow reassignment of function parameters
+        // disallow parameter object manipulation except for specific exclusions
+        'no-param-reassign': ['error', {
+            props: true,
+            ignorePropertyModificationsFor: [
+                'state', // for vuex state
+                'acc', // for reduce accumulators
+                'e' // for e.returnvalue
+            ]
+        }],
+        // allow optionalDependencies
+        'import/no-extraneous-dependencies': ['error', {
+            optionalDependencies: ['test/unit/index.js']
+        }],
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // 禁止将 await 写在循环里，因为这样就无法同时发送多个异步请求了
+        // @off 要求太严格了，有时需要在循环中写 await
+        'no-await-in-loop': 'off',
+        // 禁止与负零进行比较
+        'no-compare-neg-zero': 'error',
+        // 禁止在 if, for, while 里使用赋值语句，除非这个赋值语句被括号包起来了
+        'no-cond-assign': [
+            'error',
+            'except-parens'
+        ],
+        // 禁止使用 console
+        // @off console 的使用很常见
+        'no-console': 0,
+        // 禁止将常量作为 if, for, while 里的测试条件，比如 if (true), for (;;)，除非循环内部有 break 语句
+        'no-constant-condition': [
+            'error',
+            {
+                checkLoops: false
+            }
+        ],
+        // 禁止在正则表达式中出现 Ctrl 键的 ASCII 表示，即禁止使用 /\x1f/
+        // 开启此规则，因为字符串中一般不会出现 Ctrl 键，所以一旦出现了，可能是一个代码错误
+        'no-control-regex': 'error',
+        // @fixable 禁止使用 debugger
+        'no-debugger': 'error',
+        // 禁止在函数参数中出现重复名称的参数
+        'no-dupe-args': 'error',
+        // 禁止在对象字面量中出现重复名称的键名
+        'no-dupe-keys': 'error',
+        //缩进风格，可以自由缩进
+        "indent": 0,
+        // 允许三目运算中使用布尔值
+        'no-unneeded-ternary': 0,
+        // 允许使用表达式
+        'no-unused-expressions': 0,
     }
-  },
-  // add your custom rules here
-  rules: {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-      vue: 'never'
-    }],
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    'no-param-reassign': ['error', {
-      props: true,
-      ignorePropertyModificationsFor: [
-        'state', // for vuex state
-        'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      optionalDependencies: ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
 }
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+/* eslint-disable import/extensions */
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+    routes: [
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld,
+        },
+    ],
+});
