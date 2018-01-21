8630aedd0211c3c4ac6b0ddb5cc4eecbca43d7f8
xunmi <xunmi1@outlook.com>
2018-01-21 15:30:35 +0800

    背景组件添加

--- client/src/components/TheIndex.vue
+++ client/src/components/TheIndex.vue
@@ -5,7 +5,7 @@
             <h1>{{ title }}</h1>
             <div class="header-content">
                 <div class="item1">
-                    <img src="../assets/images/github.png" alt="github" height="30" width="30"/>
+                    <img src="../assets/images/github.png" alt="github" height="36" width="36"/>
                 </div>
                 <div class="item2">
                     <h2>
@@ -42,9 +42,11 @@
     h1, h2 {
         font-weight: normal;
     }
-
+    h2 {
+        font-size: 26px;
+    }
     .container-header h1 {
-        padding-top: 48px;
+        padding-top: 56px;
         margin-bottom: 12px;
     }
 
