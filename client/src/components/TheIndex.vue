5f5869d880c4835bc1584b99924ea350159bf2f5
xunmi <xunmi1@outlook.com>
2018-01-22 15:22:09 +0800

    路由问题解决

--- client/src/components/TheIndex.vue
+++ client/src/components/TheIndex.vue
@@ -15,46 +15,74 @@
             </div>
         </div>
         <div class="container-content">
-            <the-three-input v-bind:InputAttribute="types"></the-three-input>
+            <router-view v-bind:InputAttribute="types"/>
         </div>
     </div>
 </template>
 
 <script>
     import TheBackground from './TheBackground';
-    import TheThreeInput from './TheThreeInput';
-    let InputAttribute = {
+    //import TheThreeInput from './TheThreeInput';
+    let InputButton = {
         input1: {
-            type: 'text',
+            type: 'button',
             value: '进入',
-            placeholder: '进入',
-            name:''
         },
         input2: {
             type: 'button',
             value: '登录',
-            placeholder: '登录',
-            name:''
         },
         input3: {
             type: 'button',
             value: '注册',
-            placeholder: '注册',
-            name:''
-        }
     }
+    };
+    // let InputLogin = {
+    //     input1: {
+    //         type: 'text',
+    //         placeholder: '用户名',
+    //         name: 'username'
+    //     },
+    //     input2: {
+    //         type: 'password',
+    //         placeholder: '密码',
+    //         name: 'pwd'
+    //     },
+    //     input3: {
+    //         type: 'submit',
+    //         placeholder: '登录',
+    //         name: 'login'
+    //     }
+    // };
+    // let InputRegistration = {
+    //     input1: {
+    //         type: 'text',
+    //         placeholder: '邮箱',
+    //         name: 'username'
+    //     },
+    //     input2: {
+    //         type: 'password',
+    //         placeholder: '密码',
+    //         name: 'pwd'
+    //     },
+    //     input3: {
+    //         type: 'submit',
+    //         placeholder: '注册',
+    //         name: 'registration'
+    //     }
+    // };
     export default {
         name: 'TheIndex',
         data() {
             return {
-                types: InputAttribute,
+                types: InputButton,
                 title: '软件 BUG 跟踪管理系统',
                 githuburl: 'https://github.com/xunmi1/bug-management-system'
             };
         },
         components: {
             'the-background': TheBackground,
-            'the-three-input': TheThreeInput
+            //'the-three-input': TheThreeInput
         }
     };
 </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import TheIndex from '@/components/TheIndex';
+import  TheThreeInput from '@/components/TheThreeInput';
 
 Vue.use(Router);
 
@@ -8,9 +9,18 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'TheIndex',
+            name: 'index',
             // 注册将使用的组件（局部注册），只在父组件可用
-            component: TheIndex
+            component: TheIndex,
+            children: [
+                {
+                    path:'home',
+                    name:'home',
+                    component: TheThreeInput,
+                    // 别名，访问 / 时转向 /home, 且 url 不变。不同于重定向
+                    alias: '/'
+                }
+            ]
         }
     ]
 });
