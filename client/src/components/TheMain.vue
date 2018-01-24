f57f45d28694f02b8a25608a7c9177ea1bd8f11b
xunmi <xunmi1@outlook.com>
2018-01-25 02:58:16 +0800

    组件部分问题解决

--- client/src/components/TheMain.vue
+++ client/src/components/TheMain.vue
@@ -9,46 +9,46 @@
                         </a>
                     </div>
                     <div class="menu-search">
-                        <Input v-model="value4" icon="ios-clock-outline" placeholder="Enter something..."
+                        <Input v-model="SearchValue" icon="ios-search-strong" placeholder="搜索..."
                                style="width: 200px"></Input>
                     </div>
-                    <MenuItem name="1">
-                        <Icon type="ios-paper"></Icon>
-                        内容管理
-                    </MenuItem>
-                    <MenuItem name="2">
-                        <Icon type="ios-people"></Icon>
-                        用户管理
-                    </MenuItem>
-                    <Submenu name="3">
-                        <template slot="title">
-                            <Icon type="stats-bars"></Icon>
-                            统计分析
-                        </template>
-                        <MenuGroup title="使用">
-                            <MenuItem name="3-1">新增和启动</MenuItem>
-                            <MenuItem name="3-2">活跃分析</MenuItem>
-                            <MenuItem name="3-3">时段分析</MenuItem>
-                        </MenuGroup>
-                        <MenuGroup title="留存">
-                            <MenuItem name="3-4">用户留存</MenuItem>
-                            <MenuItem name="3-5">流失用户</MenuItem>
-                        </MenuGroup>
-                    </Submenu>
-                    <MenuItem name="4">
-                        <Icon type="settings"></Icon>
-                        综合设置
-                    </MenuItem>
-                    <div class="menu-user">
-                        <Badge dot>
+                    <div class="menu-nav">
+                        <MenuItem name="1">
+                            <Icon type="ios-paper"></Icon>
+                            内容管理
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-people"></Icon>
+                            用户管理
+                        </MenuItem>
+                        <Submenu name="3">
+                            <template slot="title">
+                                <Icon type="stats-bars"></Icon>
+                                统计分析
+                            </template>
+                            <MenuGroup title="使用">
+                                <MenuItem name="3-1">新增和启动</MenuItem>
+                                <MenuItem name="3-2">活跃分析</MenuItem>
+                                <MenuItem name="3-3">时段分析</MenuItem>
+                            </MenuGroup>
+                            <MenuGroup title="留存">
+                                <MenuItem name="3-4">用户留存</MenuItem>
+                                <MenuItem name="3-5">流失用户</MenuItem>
+                            </MenuGroup>
+                        </Submenu>
+                        <MenuItem name="4">
+                            <Icon type="settings"></Icon>
+                            综合设置
+                        </MenuItem>
+                        <Badge dot style="margin: 0 20px">
                             <Avatar shape="square" icon="person"/>
                         </Badge>
                     </div>
                 </Menu>
             </Header>
-            <Layout style="margin-top: 60px;">
+            <Layout style="margin-top: 60px">
                 <Sider hide-trigger :style="{background: '#fff'}">
-                    <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+                    <Menu theme="light" width="auto">
                         <Submenu name="1">
                             <template slot="title">
                                 <Icon type="ios-navigate"></Icon>
@@ -93,12 +93,22 @@
 <script>
     import Header from "iview/src/components/layout/header";
 
-    export default {components: {Header}}
+    export default {
+        data() {
+            return {
+                SearchValue: ''
+            };
+            components: {
+                Header
+            }
+        }
+    }
 </script>
 <style scoped>
+
     .ivu-layout-header {
         background: #fff;
-        padding: 0 40px;
+        padding: 0 24px;
         height: 60px;
         line-height: 60px;
         position: fixed;
@@ -108,35 +118,42 @@
         z-index: 1000;
     }
 
-    .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item-active {
+    .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item,
+    .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu,
+    .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item:hover,
+    .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu:hover {
         border-bottom-width: 3px;
     }
 
     .menu-logo {
         float: left;
         font-size: 22px;
+        margin-right: 60px;
     }
 
     .menu-logo a img {
-        margin-right: 10px;
+        margin-right: 8px;
         vertical-align: middle;
     }
 
     .menu-search {
         float: left;
-        margin: 0 60px;
+        margin-right: 20px;
     }
 
-    .menu-user {
+    .menu-nav {
         float: right;
+        height: 60px;
     }
+
     .ivu-layout-sider {
         position: fixed;
         height: 100vh;
         left: 0;
         overflow: auto;
     }
-    .ivu-layout {
-        height: 100vh;
+
+    .ivu-layout-content {
+        height: 77vh;
     }
 </style>
