bb288273fb425cc88debf20b61b862363fa3fe43
xunmi <xunmi1@outlook.com>
2018-02-10 23:59:26 +0800

    mssql 连接初始化

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 // require('koa-router')返回的是函数，需要()来调用函数
 const router = require('koa-router')();
-
+const db = require('../databases/mssql');
 // add url-route:
 router.get('/hello/:name', async (ctx, next) => {
     var name = ctx.params.name;
@@ -11,6 +11,8 @@ router.get('/index', async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
     ctx.response.body = '<h1>Hello, koa2!</h1>';
+
+    db();
 });
 
 module.exports = router;
