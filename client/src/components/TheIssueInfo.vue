2e11412e07005b88e5daf472b77a5a5eeeccec92
xunmi <xunmi1@outlook.com>
2018-02-14 02:30:59 +0800

    issuePlan 组件 - 布局内容完善

--- client/src/components/TheIssueInfo.vue
+++ client/src/components/TheIssueInfo.vue
@@ -1,59 +1,73 @@
 <template>
-    <Form :model="issueInfo" :label-width="70">
+    <Form :model="issueInfo" :label-width="74">
         <Row>
-            <Col span="18">
-            <FormItem label="标题">
-                <Input v-model="issueInfo.title" placeholder="问题简述......"></Input>
-            </FormItem>
-            </Col>
-            <Col span="6">
-            <FormItem label="类型">
-                <Select v-model="issueInfo.select">
-                    <Option value="bug">缺陷</Option>
-                    <Option value="task">任务</Option>
-                    <Option value="require">需求</Option>
-                    <Option value="optimize">优化</Option>
-                </Select>
-            </FormItem>
-            </Col>
+            <iCol span="18">
+                <FormItem label="标题">
+                    <Input v-model="issueInfo.title" placeholder="问题简述......"></Input>
+                </FormItem>
+            </iCol>
+            <iCol span="6">
+                <FormItem label="类型">
+                    <Select v-model="issueInfo.select">
+                        <Option value="bug">缺陷</Option>
+                        <Option value="task">任务</Option>
+                        <Option value="require">需求</Option>
+                        <Option value="optimize">优化</Option>
+                    </Select>
+                </FormItem>
+            </iCol>
         </Row>
         <Row>
-            <Col span="12">
-            <FormItem label="所属项目">
-                <Select v-model="issueInfo.project">
-                    <Option value="project1">未知</Option>
-                </Select>
-            </FormItem>
-            </Col>
-            <Col span="12">
-            <FormItem label="模块">
-                <Select v-model="issueInfo.module">
-                    <Option value="module1">未知</Option>
-                </Select>
-            </FormItem>
-            </Col>
+            <iCol span="12">
+                <FormItem label="所属项目">
+                    <Select v-model="issueInfo.project">
+                        <Option value="project1">未知</Option>
+                    </Select>
+                </FormItem>
+            </iCol>
+            <iCol span="12">
+                <FormItem label="模块">
+                    <Select v-model="issueInfo.module">
+                        <Option value="module1">未知</Option>
+                    </Select>
+                </FormItem>
+            </iCol>
+        </Row>
+        <Row>
+            <iCol span="14">
+                <FormItem label="严重程度">
+                    <Slider v-model="issueInfo.severity" :max="4" :tip-format="tipFormat" show-stops></Slider>
+                </FormItem>
+            </iCol>
+            <iCol span="10">
+                <FormItem label="版本号">
+                    <AutoComplete v-model="issueInfo.version" :data="versionData"
+                                  @on-search="versionSearch" placeholder="问题所在版本">
+                    </AutoComplete>
+                </FormItem>
+            </iCol>
         </Row>
         <FormItem label="详细描述">
             <Input v-model="issueInfo.textarea" type="textarea"
-                   :autosize="{minRows: 4,maxRows: 10}"></Input>
+                   :autosize="{minRows: 4, maxRows: 12}"></Input>
         </FormItem>
         <FormItem label="附件">
             <Row>
-                <Col span="6">
-                <Upload multiple action="">
-                    <Button type="ghost" icon="ios-cloud-upload-outline">图片上传</Button>
-                </Upload>
-                </Col>
-                <Col span="6">
-                <Upload multiple action="">
-                    <Button type="ghost" icon="ios-cloud-upload-outline">文本上传</Button>
-                </Upload>
-                </Col>
-                <Col span="6">
-                <Upload multiple action="">
-                    <Button type="ghost" icon="ios-cloud-upload-outline">其他附件</Button>
-                </Upload>
-                </Col>
+                <iCol span="6">
+                    <Upload multiple action="">
+                        <Button type="ghost" icon="ios-cloud-upload-outline">图片上传</Button>
+                    </Upload>
+                </iCol>
+                <iCol span="6">
+                    <Upload multiple action="">
+                        <Button type="ghost" icon="ios-cloud-upload-outline">文本上传</Button>
+                    </Upload>
+                </iCol>
+                <iCol span="6">
+                    <Upload multiple action="">
+                        <Button type="ghost" icon="ios-cloud-upload-outline">其他附件</Button>
+                    </Upload>
+                </iCol>
             </Row>
         </FormItem>
     </Form>
@@ -67,10 +81,35 @@
                 issueInfo: {
                     title: '',
                     select: 'bug',
+                    severity: 0,
+                    version: '',
                     project: '',
                     module: '',
                     textarea: ''
+                },
+                versionData: []
+            }
+        },
+        methods: {
+            tipFormat(val) {
+                switch (val) {
+                    case 0:
+                        return '极低';
+                    case 1:
+                        return '低';
+                    case 2:
+                        return '中';
+                    case 3:
+                        return '高';
+                    case 4:
+                        return '极高';
                 }
+            },
+            versionSearch(val) {
+                this.versionData = !val ? [] : [
+                    value,
+                    val + '.1'
+                ];
             }
         }
     }
--- client/src/components/TheIssuePlan.vue
+++ client/src/components/TheIssuePlan.vue
@@ -1,10 +1,122 @@
 <template>
-
+    <Form :model="issuePlan" :label-width="78">
+        <Row>
+            <iCol span="12">
+                <FormItem label="分配人员">
+                    <AutoComplete v-model="issuePlan.dispense"
+                                  :data="dispenseData"
+                                  :disabled="disabledState1"
+                                  @on-search="dispenseSearch"
+                                  placeholder="分配此问题的人员">
+                    </AutoComplete>
+                </FormItem>
+            </iCol>
+            <iCol span="12">
+                <FormItem label="解决人员">
+                    <AutoComplete v-model="issuePlan.handle"
+                                  :data="handleData"
+                                  :disabled="disabledState2"
+                                  @on-search="handleSearch"
+                                  placeholder="解决此问题的人员">
+                    </AutoComplete>
+                </FormItem>
+            </iCol>
+        </Row>
+        <Row>
+            <iCol span="14">
+                <FormItem label="优先级">
+                    <Slider v-model="issuePlan.priority" :max="4" :tip-format="tipFormat" show-stops></Slider>
+                </FormItem>
+            </iCol>
+            <iCol span="10">
+                <FormItem label="版本号">
+                    <AutoComplete v-model="issuePlan.versionEnd" :data="versionEndData"
+                                  @on-search="versionSearch" placeholder="预计解决时的版本">
+                    </AutoComplete>
+                </FormItem>
+            </iCol>
+        </Row>
+        <Row>
+            <iCol span="12">
+                <FormItem label="起始日期">
+                    <DatePicker v-model="issuePlan.dateState" format="yyyy年 MM月 dd日" style="width: 100%"
+                                placeholder="选择日期"></DatePicker>
+                </FormItem>
+            </iCol>
+            <iCol span="12">
+                <FormItem label="截止日期">
+                    <DatePicker v-model="issuePlan.dateEnd" format="yyyy年 MM月 dd日" style="width: 100%"
+                                placeholder="选择日期"></DatePicker>
+                </FormItem>
+            </iCol>
+        </Row>
+    </Form>
 </template>
 
 <script>
     export default {
-        name: "theIssuePlan"
+        name: "theIssuePlan",
+        data() {
+            return {
+                issuePlan: {
+                    dispense: '',
+                    handle: '',
+                    priority: 2,
+                    versionEnd: '',
+                    dateState: '',
+                    dateEnd: ''
+                },
+                dispenseData: [],
+                handleData: [],
+                versionEndData: [],
+                disabledState1: false,
+                disabledState2: false,
+            }
+        },
+        methods: {
+            dispenseSearch(val) {
+                if (val) {
+                    this.disabledState2 = true;
+                } else {
+                    this.disabledState2 = false;
+                }
+                this.handleData = !val ? [] : [
+                    val,
+                    val + '测试'
+                ];
+            },
+            handleSearch(val) {
+                if (val) {
+                    this.disabledState1 = true;
+                } else {
+                    this.disabledState1 = false;
+                }
+                this.handleData = !val ? [] : [
+                    val,
+                    val + '测试'
+                ];
+            },
+            tipFormat(val) {
+                switch (val) {
+                    case 0:
+                        return '极低';
+                    case 1:
+                        return '低';
+                    case 2:
+                        return '中';
+                    case 3:
+                        return '高';
+                    case 4:
+                        return '紧急';
+                }
+            },
+            versionSearch(val) {
+                this.versionData = !val ? [] : [
+                    value,
+                    val + '.1'
+                ];
+            }
+        }
     }
 </script>
 
