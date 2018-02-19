0567f24ab92d891bcd4071b29ae3b8ba160978a6
xunmi <xunmi1@outlook.com>
2018-02-19 21:00:12 +0800

    删除 card 组件

--- client/src/components/TheProject.vue
+++ client/src/components/TheProject.vue
@@ -3,56 +3,57 @@
         <Panel name="1">
             拥有的项目
             <div slot="content">
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
+                <Card class="card">
+                    <p slot="title">Borderless card</p>
+                    <p>Content of card</p>
+                    <p>Content of card</p>
+                    <p>Content of card</p>
+                </Card>
+                <Card class="card">
+                    <p slot="title">Borderless card</p>
+                    <p>Content of card</p>
+                    <p>Content of card</p>
+                    <p>Content of card</p>
+                </Card>
             </div>
         </Panel>
         <Panel name="2">
             参与的项目
             <div slot="content">
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
+
             </div>
         </Panel>
         <Panel name="3">
             已关闭项目
             <div slot="content">
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
+
             </div>
         </Panel>
         <Panel name="4">
             删除的项目
             <div slot="content">
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
-                <base-card></base-card>
+
             </div>
         </Panel>
     </Collapse>
 </template>
 
 <script>
-    import BaseCard from './BaseCard';
     export default {
         name: "TheProject",
         data() {
-            return {
-            }
-        },
-        components: {
-            BaseCard,
-            'base-card': BaseCard
+            return {}
         }
     }
 </script>
 
 <style scoped>
+    .card {
+        width: 180px;
+        height: 180px;
+        border: 1px solid #2d8cf0;
+        margin: 14px;
+        background-color: rgba(220, 220, 220, 0.3);
+        float: left;
+    }
 </style>
